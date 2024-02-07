c98baf36fc19b5420276aadf43822bbc249245bb
Catron-Gainey <catrong23@gmail.com>
2024-02-07 08:41:27 -0500

    updated comments

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -10,7 +10,7 @@ bcrypt = Bcrypt(app)
 # Remember 'fat models, skinny controllers' more logic should go in here rather than in your controller. Your controller should be able to just call a function from the model for what it needs, ideally.
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 class Person:
-    db = "people" #which database are you using for this project
+    db = "people" 
     def __init__(self, data):
         self.id = data['id']
         self.first_name = data['first_name']
@@ -19,8 +19,6 @@ class Person:
         self.password = data['password']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-        # What changes need to be made above for this project?
-        #What needs to be added here for class association?
 
 
 
@@ -43,13 +41,6 @@ class Person:
             return False
         return cls(result[0])
 
-
-
-    # Update Users Models
-
-
-
-    # Delete Users Models
 
     @staticmethod
     def validate_user(user):
